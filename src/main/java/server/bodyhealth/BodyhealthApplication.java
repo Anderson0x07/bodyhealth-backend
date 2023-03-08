3a8437a64da326f52452fcf3ae5d35fe276d7b85
Anderson0x07 <andersonadrianorro@ufps.edu.co>
2023-03-07 20:19:27 -0500

    init repository

---
+++ src/main/java/server/bodyhealth/BodyhealthApplication.java
@@ -0,0 +1,13 @@
+package server.bodyhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyhealthApplication.class, args);
+	}
+
+}
